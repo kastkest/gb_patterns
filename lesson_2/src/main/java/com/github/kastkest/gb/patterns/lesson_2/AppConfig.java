7a170c690f9d50b9a437db02b52c955dc13b9011
kastkest <endimion-xi@yandex.ru>
2022-12-11 22:46:54 +0400

    feat: create HW2: decomposed WebServer

---
+++ lesson_2/src/main/java/com/github/kastkest/gb/patterns/lesson_2/AppConfig.java
@@ -0,0 +1,6 @@
+package com.github.kastkest.gb.patterns.lesson_2;
+
+public class AppConfig {
+    public static final String WWW = "/Users/aleks/dev/geek-architecture-01/www";
+    public static final int PORT = 8080;
+}
---
+++ lesson_2/src/main/java/com/github/kastkest/gb/patterns/lesson_2/WebServer.java
@@ -0,0 +1,23 @@
+package com.github.kastkest.gb.patterns.lesson_2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(AppConfig.PORT)) {
+            System.out.println("Server started!");
+            RequestParser requestParser = new RequestParser();
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected!");
+
+                new Thread(new RequestHandler(new SocketService(socket), requestParser)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
