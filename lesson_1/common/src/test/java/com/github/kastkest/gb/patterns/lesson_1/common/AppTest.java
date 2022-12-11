af49fdd5a22d372292af6bf578ff0be85e6c2d4c
kastkest <endimion-xi@yandex.ru>
2022-12-11 16:06:50 +0400

    init: create patterns project

---
+++ lesson_1/common/src/test/java/com/github/kastkest/gb/patterns/lesson_1/common/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.kastkest.gb.patterns.lesson_1.common;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
